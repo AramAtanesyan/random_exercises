1537bc13b1694ebc37b090666fd5ae1d9f2cc349
âAram <a.g.atanesyan@gmail.com>
2022-04-08 18:08:33 +0400

    changes

--- randomTestings.js
+++ randomTestings.js
@@ -3,6 +3,7 @@ function arraySubset(source, subset) {
         return false;
     }
 
+
     for(let i = 0; i < subset.length; i++) {
         if(source.includes(subset[i])) {
             source = getTheSameArrayExceptValue(source, subset[i]);
@@ -17,9 +18,6 @@ function getTheSameArrayExceptValue(source, valueToDelete) {
     source.splice(source.indexOf(valueToDelete), 1);
     return source;
 }
-
-
-
 
 console.log(arraySubset([2, 1, 3], [1, 2])) // -> true
 console.log(arraySubset([2, 1, 1, 3], [1, 2, 3])) // -> true
